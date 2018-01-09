bc045d11485a59e89a3ff46a9f716056e6833e88
Elena <elenacerezo83@gmail.com>
2018-01-09 03:22:52 +0100

    api request

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 'use strict';
-
+//  MENÚ BURGUER O TRIGGER
+//  Variables
 var page = document.querySelector('.burguer-display');
 var menuTrigger = page.querySelector('.burguer-opener');
 var menuCloseBTN = page.querySelector('.burguer-close');
 
-// Functions
+// Funciones
 function openNavMenu() {
 	page.classList.add('page--menu-visible');
 };
@@ -14,6 +15,35 @@ function closeNavMenu() {
 };
 
 
-// Magic
+// Eventos
 menuTrigger.addEventListener('click', openNavMenu);
 menuCloseBTN.addEventListener('click', closeNavMenu);
+
+// PARTE AJAX: Cargar contenido nuevo desde una API
+// Variables
+var moreReasonsButton = document.querySelector('.button-opposite');
+var reasonsContainer = document.querySelector('.reasons-grid');
+var moreReasons = '';
+var evenMoreReasons ='';
+// Funciones
+function getRandomReasons(){
+ var request = new XMLHttpRequest();
+ request.open('GET',' https://three-random-reasons-izwfjpgbqm.now.sh/');
+ request.addEventListener('load',addRandomReasons);
+
+		function addRandomReasons(){
+			var response = request.responseText;
+			var reasonsJson = JSON.parse(response);
+
+		for (var i = 0; i < reasonsJson.reasons.length; i++) {
+			moreReasons += '<div class="row"><h6 class="font-section-reasons-subtitle">'+ reasonsJson.reasons[i].title +
+     '</h6><p class="font-section-reasons-description">'+ reasonsJson.reasons[i].description + '</p></div>';
+     evenMoreReasons = reasonsContainer.innerHTML + moreReasons;
+		}
+reasonsContainer.innerHTML = evenMoreReasons;
+}
+request.send();
+}
+
+// Eventos
+moreReasonsButton.addEventListener('click', getRandomReasons);
