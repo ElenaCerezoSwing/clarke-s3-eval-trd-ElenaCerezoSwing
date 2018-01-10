68bcdc90e27377ab6adf4c73d2d51c3c3889064c
Elena <elenacerezo83@gmail.com>
2018-01-10 13:32:43 +0100

    maybe it's final version

--- js/main.min.js
+++ js/main.min.js
@@ -23,8 +23,8 @@ menuCloseBTN.addEventListener('click', closeNavMenu);
 // Variables
 var moreReasonsButton = document.querySelector('.button-opposite');
 var reasonsContainer = document.querySelector('.reasons-grid');
-var moreReasons = '';
-var evenMoreReasons ='';
+// var moreReasons = '';
+// var evenMoreReasons ='';
 // Funciones
 function getRandomReasons(){
  var request = new XMLHttpRequest();
@@ -36,34 +36,12 @@ function getRandomReasons(){
 			var reasonsJson = JSON.parse(response);
 
 		for (var i = 0; i < reasonsJson.reasons.length; i++) {
-      //  Y aquí hay varios caminos que hay que preguntar a Álex:
-// 1º un pelín fallido, pero bueno si encontramos el fallo: (es el caso i*3)
-
-		// 	moreReasons += '<div class="row"><h6 class="font-section-reasons-subtitle">'+ reasonsJson.reasons[i].title +
-    //  '</h6><p class="font-section-reasons-description">'+ reasonsJson.reasons[i].description + '</p></div>';
-    //  evenMoreReasons = reasonsContainer.innerHTML + moreReasons ;
-
-// después del cierre del for deberíamos decir que:
-// reasonsContainer.innerHTML = evenMoreReasons;
-
-
-// 2º también fallido, lo hace bien pero nos elimina nuestra primera opción... (la que hemos escrito a mano):
-
-// 	moreReasons += '<div class="row"><h6 class="font-section-reasons-subtitle">'+ reasonsJson.reasons[i].title +
-//  '</h6><p class="font-section-reasons-description">'+ reasonsJson.reasons[i].description + '</p></div>';
-//  evenMoreReasons = reasonsContainer.innerHTML + moreReasons ;
-
-// después del cierre del for deberíamos decir que:
-// reasonsContainer.innerHTML = moreReasons;
-
-// y el 3º (que es el más eficiente y sencillo)
-
 
      reasonsContainer.innerHTML += '<div class="row"><h6 class="font-section-reasons-subtitle">'+ reasonsJson.reasons[i].title +
 		'</h6><p class="font-section-reasons-description">'+ reasonsJson.reasons[i].description + '</p></div>';
 
 		}
-// reasonsContainer.innerHTML = evenMoreReasons;
+
 }
 request.send();
 }
